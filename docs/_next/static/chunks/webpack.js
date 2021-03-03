644a102d0c64241f2d7dff5b1899c9691e73c40e
Christian Broms <cb@christianbroms.com>
2021-03-02 23:15:39 -0500

    add post mordem post

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5518b74d7a36c0aa308a";
+/******/ 	var hotCurrentHash = "bae9869e7235939b14d1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
